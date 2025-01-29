f65e36bc680b3b7a7c496be8a8a8697a78bcf276
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-28 22:27:12 -0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 
 const App = () => {
