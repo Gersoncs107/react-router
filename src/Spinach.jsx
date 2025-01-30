a98c82222b03b779e44180ee540541130f8b0a6c
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-30 19:17:22 -0300

    react

--- src/Spinach.jsx
+++ src/Spinach.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const Spinatch = () => {
     return(
         <div>
-            
+            <p>Hi, I am Spinach! Popeye loves to eat me!</p>
+            <Link to="/">Click here to go back</Link>
         </div>
     )
 }
+
+export default Spinatch
