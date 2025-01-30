1697657104e17749ae979edf22937a48e28466c0
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-30 19:14:29 -0300

    react

--- src/Popeye.jsx
+++ src/Popeye.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom"
 const Popeye = () => {
     return(
         <div>
-            
+            <p>HI, i'm Popeye! I love to eat spinatch!</p>
+            <Link to="/">Click here to go back</Link>
         </div>
     )
 }
+
+export default Popeye
