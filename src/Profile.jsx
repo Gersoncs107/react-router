5ebc598680254f4b2615ffa0ceb729227ff8438c
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-30 19:28:03 -0300

    react

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -5,6 +5,9 @@ const Profile = () => {
         <div>
             <h1>Hello from profile page!</h1>
             <p>So, how are you?</p>
+            <hr />
+            <h2>The profile visited is here:</h2>
+            <Outlet/>
         </div>
     )
 }
