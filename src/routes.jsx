2dde1714f473af7ded5aa69ed5829780406dc572
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-31 22:22:40 -0300

    react

---
+++ src/routes.jsx
@@ -0,0 +1,15 @@
+import App from "./App";
+import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+]);
